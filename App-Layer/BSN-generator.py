a5c4c77bec6487e8597d105dd674d4cbbfb8b306
Dogefreak <60531982+dogefreak@users.noreply.github.com>
2021-03-31 16:00:20 +0200

    Add files via upload

--- App-Layer/BSN-generator.py
+++ App-Layer/BSN-generator.py
@@ -4,55 +4,44 @@
 import random
 
 #Generate random numbers
-def bsnGen():
-    while True:
-        chars = random.sample(range(0, 9), 9)
+def bsnGen(number):
+    for x in range(number):
+        while True:
+            chars = random.sample(range(0, 9), 9)
+            som = 9*chars[0] + 8*chars[1] + 7*chars[2] + 6*chars[3] + 5*chars[4] + 4*chars[5] + 3*chars[6] + 2*chars[7] + -1*chars[8]
+            maths = som % 11
+            if maths == 0:
+                print("\n", *chars, sep="")
+                break
 
-        a = chars[0] * 9
-        b = chars[1] * 8
-        c = chars[2] * 7
-        d = chars[3] * 6
-        e = chars[4] * 5
-        f = chars[5] * 4
-        g = chars[6] * 3
-        h = chars[7] * 2
-        i = chars[8] * -1
-
-        som = a+b+c+d+e+f+g+h+i
-
-        maths = som % 11
-
-        if maths == 0:
-            print("\nSucces!\n")
-            print(*chars)
-            break
-        else: continue
-
+#Verify the answer of any question
 def check(question):
     while True:
         check1 = input(question + " ([y]/n): ")
-        if check1 == "y":
-            return True
-        elif check1 == "n":
-            return False
-        elif check1 == "":
-            return True
-        else:
-            print('\nThe question must be answered with "y" or "n"!')
+        if check1 == "y": return True
+        elif check1 == "n": return False
+        elif check1 == "": return True
+        else: print('\nThe question must be answered with "y" or "n"!')
 
-def main():
-    firsttry = check("Welcome to the BSN genarator, do you want one?")
-    if firsttry is True: bsnGen()
-    else:
-        print("Ciao!")
-        exit()
-    
+#Check if input is an int
+def intCheck():
     while True:
-        secondtry = check("\nDo you want another one?")
-        if secondtry is True: bsnGen()
-        else:
-            print("Ciao!")
+        number = input("\nHow many BSNs do you want?: ")
+        if number.isdigit():
+            number = int(number)
+            bsnGen(number)
             break
+        else: print("\nPlease try again!")        
+
+#Main function
+def main():
+    print("Welcome to the BSN genarator!")
+    while True:
+        tryit = check("\nDo you want one?")
+        if tryit is True:
+            intCheck()
+        else: break
+    print("\nCiao!")
     exit()
 
 main()
