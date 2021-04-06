a485da8f757ef497b3c7502367c4916f33630800
Dogefreak <60531982+dogefreak@users.noreply.github.com>
2021-04-06 13:20:06 +0200

    Changed script
    Shorter and more efficient code

--- App-Layer/Passwordchecker.py
+++ App-Layer/Passwordchecker.py
@@ -2,51 +2,29 @@
 def password_check(passwd): 
       
     SpecialSym =['$', '@', '#', '%', '!', '_'] 
-    valid = True
+    valid = False
     errorstring = "\nYour password should at least contain: "
       
-    if len(passwd) < 6:
-        errorstring = errorstring + '6 characters, ' 
-        valid = False
-          
-    if len(passwd) > 20:
-        errorstring = errorstring + 'less than 20 characters, '
-        valid = False
-          
-    if not any(char.isdigit() for char in passwd):
-        errorstring = errorstring + 'one numeral, ' 
-        valid = False
-          
-    if not any(char.isupper() for char in passwd): 
-        errorstring = errorstring + 'an uppercase letter, '
-        valid = False
-          
-    if not any(char.islower() for char in passwd): 
-        errorstring + 'a lowercase letter, '
-        valid = False
-          
-    if not any(char in SpecialSym for char in passwd): 
-        charlist = ''.join(SpecialSym)
-        errorstring = errorstring + 'special characters like "' + charlist + '"'
-        valid = False
-        
-    if valid: 
-        return valid
-    if valid is False:
-        print(errorstring + "...")
+    if len(passwd) < 6: errorstring = errorstring + '6 characters, '
+    if len(passwd) > 20: errorstring = errorstring + 'less than 20 characters, '
+    if not any(char.isdigit() for char in passwd): errorstring = errorstring + 'one numeral, ' 
+    if not any(char.isupper() for char in passwd): errorstring = errorstring + 'an uppercase letter, '
+    if not any(char.islower() for char in passwd): errorstring + 'a lowercase letter, '
+    if not any(char in SpecialSym for char in passwd): errorstring = errorstring + 'special characters like "' + ''.join(SpecialSym) + '"'
+    if errorstring == "\nYour password should at least contain: ": valid = True
+    if valid is False: print(errorstring + "...")
+    return valid   
   
-# Main method 
+# Main method
 def main():
-    invalid = True
     passwd = input("Tell me your secrets: ") 
     
-    while invalid: 
+    while True: 
         if (password_check(passwd)): 
             print("\nPassword is valid <3")
-            invalid = False
             break
         else: 
             print("Your password is invalid! :(")
-            passwd = input("\nTry again: ") 
+            passwd = input("\nTry again: ")
           
 main() 
